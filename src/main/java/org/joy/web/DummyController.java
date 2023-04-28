6841c4fca866bb7a556baf605d02ab058aa07408
sunwoong <sunwoong1122@gmail.com>
2023-04-28 21:45:46 +0900

    feat: dummy controller 구현

---
+++ src/main/java/org/joy/web/DummyController.java
@@ -0,0 +1,22 @@
+package org.joy.web;
+
+import org.joy.web.dto.DummyResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dummy")
+public class DummyController {
+    @GetMapping("/string")
+    public String dummyString() {
+        return "dummy";
+    }
+
+    @GetMapping("/json")
+    public DummyResponse dummyJson(@RequestParam String name,
+                                   @RequestParam int age) {
+        return new DummyResponse(name, age);
+    }
+}
