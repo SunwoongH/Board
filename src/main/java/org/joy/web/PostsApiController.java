2c8aa0cb179258c911e9c073601235f488a0e790
sunwoong <sunwoong1122@gmail.com>
2023-05-02 19:50:11 +0900

    feat: 게시물 등록 & 수정 & 조회 controller 기능 구현

---
+++ src/main/java/org/joy/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package org.joy.web;
+
+import lombok.RequiredArgsConstructor;
+import org.joy.service.posts.PostsService;
+import org.joy.web.dto.request.PostsSaveRequestDto;
+import org.joy.web.dto.request.PostsUpdateRequestDto;
+import org.joy.web.dto.response.PostsResponseDto;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("api/posts")
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping
+    public Long save(@RequestBody PostsSaveRequestDto postsSaveRequestDto) {
+        return postsService.save(postsSaveRequestDto);
+    }
+
+    @PutMapping("/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto postsUpdateRequestDto) {
+        return postsService.update(id, postsUpdateRequestDto);
+    }
+
+    @GetMapping("/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
