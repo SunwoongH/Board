b1cc352a96ca22d89131a09a24f590266f2e718e
sunwoong <sunwoong1122@gmail.com>
2023-05-03 16:52:17 +0900

    test: 게시글 등록일 & 수정일 자동 등록 여부 확인 테스트 코드 작성

--- src/test/java/org/joy/domain/posts/PostsRepositoryTest.java
+++ src/test/java/org/joy/domain/posts/PostsRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,7 +23,7 @@ class PostsRepositoryTest {
 
     @DisplayName("게시글을 저장하고 조회한다.")
     @Test
-    void postTest() {
+    void postsTest() {
         // given
         Posts posts = Posts.builder()
                 .title("안녕")
@@ -39,5 +40,26 @@ class PostsRepositoryTest {
         assertThat(findPosts.getTitle()).isEqualTo("안녕");
         assertThat(findPosts.getContent()).isEqualTo("하이");
         assertThat(findPosts.getAuthor()).isEqualTo("joy");
+    }
+
+    @DisplayName("게시글 등록일 & 수정일이 자동 등록된다.")
+    @Test
+    void baseTimeEntityTest() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2023, 5, 3, 3, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("안녕")
+                .content("하이")
+                .author("joy")
+                .build());
+
+        // when
+        List<Posts> findPostsList = postsRepository.findAll();
+
+        // then
+        Posts findPosts = findPostsList.get(0);
+        System.out.println(findPosts.getCreatedDate() + ", " + findPosts.getModifiedDate());
+        assertThat(findPosts.getCreatedDate()).isAfter(now);
+        assertThat(findPosts.getModifiedDate()).isAfter(now);
     }
 }
