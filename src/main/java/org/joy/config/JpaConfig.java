01d53db7facba31db7152cf257156ca80e7e2c48
sunwoong <sunwoong1122@gmail.com>
2023-05-03 16:47:09 +0900

    feat: @EnableJpaAuditing 적용

---
+++ src/main/java/org/joy/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package org.joy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
