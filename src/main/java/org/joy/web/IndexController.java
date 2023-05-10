3c6b050ecd7851ec60f8c35670b7b694d04b0ffe
sunwoong <sunwoong1122@gmail.com>
2023-05-10 22:02:27 +0900

    feat: 게시글 수정 페이지 조회 기능 추가

--- src/main/java/org/joy/web/IndexController.java
+++ src/main/java/org/joy/web/IndexController.java
@@ -1,11 +1,14 @@
 package org.joy.web;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.joy.service.posts.PostsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -20,5 +23,12 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        log.info("id = {}", id);
+        model.addAttribute("posts", postsService.findById(id));
+        return "posts-update";
     }
 }
