422b4135221017fa97872ca558c687771e6b31ae
sunwoong <sunwoong1122@gmail.com>
2023-05-16 23:54:45 +0900

    feat: OAuth 사용자 정보 관리 service 구현

---
+++ src/main/java/org/joy/config/auth/CustomOAuth2UserService.java
@@ -0,0 +1,60 @@
+package org.joy.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.joy.config.auth.dto.OAuthAttributes;
+import org.joy.config.auth.dto.SessionUser;
+import org.joy.domain.user.User;
+import org.joy.domain.user.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private final UserRepository userRepository;
+    private final HttpSession httpSession;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        // 소셜 서비스 구분 ID
+        String registrationId = userRequest
+                .getClientRegistration()
+                .getRegistrationId();
+        // OAuth2 key
+        String userNameAttributeName = userRequest
+                .getClientRegistration()
+                .getProviderDetails()
+                .getUserInfoEndpoint()
+                .getUserNameAttributeName();
+        // OAuth2User attributes 저장
+        OAuthAttributes attributes = OAuthAttributes
+                .of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+        // 사용자 정보 save or update
+        User user = saveOrUpdate(attributes);
+        // 세션에 사용자 정보 저장
+        httpSession.setAttribute("user", new SessionUser(user));
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
+                attributes.getAttributes(),
+                attributes.getNameAttributeKey());
+    }
+
+    private User saveOrUpdate(OAuthAttributes attributes) {
+        User user = userRepository.findByEmail(attributes.getEmail())
+                .map(findUser -> findUser.update(attributes.getName(), attributes.getPicture()))
+                .orElse(attributes.toEntity());
+        return userRepository.save(user);
+    }
+}
