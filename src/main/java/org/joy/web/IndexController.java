917f15d4364e44f8353340b0a573d9731f3f38bf
sunwoong <sunwoong1122@gmail.com>
2023-05-18 12:14:29 +0900

    feat: 세션에 저장된 사용자 정보로 로그인 여부 확인하는 로직 추가

--- src/main/java/org/joy/web/IndexController.java
+++ src/main/java/org/joy/web/IndexController.java
@@ -2,21 +2,29 @@ package org.joy.web;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.joy.config.auth.dto.SessionUser;
 import org.joy.service.posts.PostsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @Slf4j
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
